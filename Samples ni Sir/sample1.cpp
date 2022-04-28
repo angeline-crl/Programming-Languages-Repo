7d7c87ddc9dccbd20d67d35444dd050345876d24
Elyzza Jane Abrigo <elyzza.abrigo@gmail.com>
2022-04-28 13:09:05 +0800

    [ADD] sample files from sir

---
+++ Samples ni Sir/sample1.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    int a,b,c,d;
+
+    //assigning variable
+    a = 1;
+    b = 2;
+    c = 3;
+
+    //actucal operation
+    d = a+b+c;
+
+    cout<<d;
+
+    return 0;
+}
+
+/*
+    operators
+        =
+        +
+    keywords
+        include
+        iostream
+        using
+        namespace
+        std
+        int
+        main
+        cout
+        return 
+    symbols
+        <
+        >
+    identifier
+        a, b, c, d
+*/
