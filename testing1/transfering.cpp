dedaa858945c41186571d82f2c32cbe33757f551
Elyzza Jane Abrigo <elyzza.abrigo@gmail.com>
2022-04-28 23:57:04 +0800

    [ADD] testing files

---
+++ testing1/transfering.cpp
@@ -0,0 +1,87 @@
+//lexcial analyzer
+#include <iostream>
+#include <cstring>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string>
+#include <fstream>
+using namespace std;
+
+string keys [100];
+
+void store_keyword(){
+    short loop=0; 
+    string line; 
+    ifstream myfile ("listOfKeywords.txt");
+    if (myfile.is_open()) 
+    {
+        while(getline(myfile,line)){
+            keys[loop] = line; 
+            loop++;
+        }
+        myfile.close(); 
+    }
+}
+
+bool isKeyword (string a)
+{
+	//gets the size of the key array
+	for (int i = 0; i < 14 ; i++) {
+		if (keys[i] == a) {
+			return true;
+		}
+	}
+	return false;
+}
+
+int main (){
+    ifstream file("prog.txt");
+    string x;
+    string code = "";
+
+	store_keyword();
+
+    while(getline(file,x)){
+        code+=x;
+    }
+
+    string s = "";
+    for(int i = 0; i<code.size(); i++){
+		if(code[i] != ' '){
+            s += code[i];
+        }
+		else {
+			if (s == "+" || s == "-" || s == "*" || s == "/" || 
+            s == "^" || s == "&&" || s == "||" || s == "=" || 
+            s == "==" || s == "&" || s == "|" || s == "%" || 
+            s == "++" || s == "--" || s == "+=" || s == "-=" || 
+            s == "/=" || s == "*=" || s == "%=") {
+                cout << s <<" is an operator"<<endl;
+                s = "";
+            } else if (isKeyword (s)){
+				cout << s <<" is a keyword"<<endl;
+				s = "";
+			} else if (s == "(" || s == "{" || s == "[" || s == ")" || 
+            s == "}" || s == "]" || s == "<" || s == ">" ||
+            s == "()" || s == ";" || s == "<<" || s == ">>" ||
+            s == "," || s == "#"){
+				cout << s <<" is a symbol"<<endl;
+				s = "";
+			} else if (s == "\n" || s == "" || s == "") {
+				s = "";
+			} else if (isdigit (s[0])) {
+				int x = 0;
+					if (!isdigit (s[x++])) {
+						continue;
+					}
+					else {
+						cout << s <<" is a constant"<<endl;
+						s = "";
+					}		
+			} else {
+				cout << s <<" is an identifier"<<endl;
+				s = "";
+			}		
+		}	
+	}
+}
